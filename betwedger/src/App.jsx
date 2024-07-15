e1a560cb73efb80c018a016cf76c5cae63b89dd7
energeticelephant <energeticelephant1998@gmail.com>
2024-07-15 17:55:06 +0800

    more or less done

--- betwedger/src/App.jsx
+++ betwedger/src/App.jsx
@@ -14,6 +14,12 @@ import {
 } from "@mui/material";
 import ResponsiveAppBar from "./components/AppBar.jsx";
 import backgroundImage from "./assets/background.png";
+import baccarat from "./assets/Baccarat-1.jpg";
+import blackjack from "./assets/Blackjack-1.jpg";
+import roulette from "./assets/Roulette-1.jpg";
+import slots from "./assets/Slots-1.jpg";
+import { Margin } from "@mui/icons-material";
+import Footer from "./components/footer.jsx";
 
 function App() {
   return (
@@ -83,7 +89,6 @@ function App() {
             color="inherit"
             textDecoration="none"
             marginTop={4}
-
           >
             Check Out Our Upcoming Games!
           </Typography>
@@ -104,17 +109,17 @@ function App() {
                   <CardMedia
                     component="img"
                     height="140"
-                    image="/static/images/cards/contemplative-reptile.jpg"
-                    alt="green iguana"
+                    image={roulette}
+                    alt="Roulette"
                   />
                   <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
-                      Lizard
+                      Roulette
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                      Lizards are a widespread group of squamate reptiles, with
-                      over 6,000 species, ranging across all continents except
-                      Antarctica
+                      A casino game where players bet on where a ball will land
+                      on a spinning wheel divided into numbered and colored
+                      sections.
                     </Typography>
                   </CardContent>
                 </CardActionArea>
@@ -126,17 +131,17 @@ function App() {
                   <CardMedia
                     component="img"
                     height="140"
-                    image="/static/images/cards/contemplative-reptile.jpg"
-                    alt="green iguana"
+                    image={baccarat}
+                    alt="Baccarat"
                   />
                   <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
-                      Lizard
+                      Baccarat
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                      Lizards are a widespread group of squamate reptiles, with
-                      over 6,000 species, ranging across all continents except
-                      Antarctica
+                      A card game where players bet on the outcome of the hand,
+                      trying to predict whether the player's or the banker's
+                      hand will be closer to a value of nine.
                     </Typography>
                   </CardContent>
                 </CardActionArea>
@@ -148,17 +153,16 @@ function App() {
                   <CardMedia
                     component="img"
                     height="140"
-                    image="/static/images/cards/contemplative-reptile.jpg"
-                    alt="green iguana"
+                    image={blackjack}
+                    alt="Blackjack"
                   />
                   <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
-                      Lizard
+                      Blackjack
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                      Lizards are a widespread group of squamate reptiles, with
-                      over 6,000 species, ranging across all continents except
-                      Antarctica
+                      A card game where players aim to have a hand value closer
+                      to 21 than the dealer's without exceeding 21.
                     </Typography>
                   </CardContent>
                 </CardActionArea>
@@ -170,17 +174,16 @@ function App() {
                   <CardMedia
                     component="img"
                     height="140"
-                    image="/static/images/cards/contemplative-reptile.jpg"
-                    alt="green iguana"
+                    image={slots}
+                    alt="Slots"
                   />
                   <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
-                      Lizard
+                      Slots
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                      Lizards are a widespread group of squamate reptiles, with
-                      over 6,000 species, ranging across all continents except
-                      Antarctica
+                      A game of chance played on a machine with spinning reels,
+                      where players win by matching symbols on pay lines.
                     </Typography>
                   </CardContent>
                 </CardActionArea>
@@ -190,9 +193,45 @@ function App() {
         </Container>
       </section>
       <section id="aboutus">
-        <h1>About Us Section</h1>
-        <p>Learn more about us here.</p>
+        <Container maxWidth="lg" sx={{marginTop:'5px'}}>
+          <Typography
+            variant="h3"
+            textAlign="center"
+            fontSize={{
+              xs: "1rem", // 1.5rem font size on extra-small screens
+              sm: "1.5rem", // 2rem font size on small screens
+              md: "1.5rem", // 2.5rem font size on medium screens
+              lg: "2rem", // 3rem font size on large screens
+              xl: "2.5rem",
+            }}
+            fontWeight={700}
+            color="inherit"
+            textDecoration="none"
+            marginTop={4}
+          >
+            About Us
+          </Typography>
+          <Typography
+            variant="body1"
+            textAlign="center"
+            fontSize={{
+              xs: "1rem", // 1.5rem font size on extra-small screens
+              sm: "1.5rem", // 2rem font size on small screens
+              md: "1.5rem", // 2.5rem font size on medium screens
+              lg: "2rem", // 3rem font size on large screens
+              xl: "2.5rem",
+            }}
+            fontWeight={400}
+            color="inherit"
+            textDecoration="none"
+            marginTop={2}
+          >
+            BETWAGER is an online platform providing casino games for your
+            entertainment. More services will be coming soon. Stay tuned!
+          </Typography>
+        </Container>
       </section>
+      <Footer />
     </div>
   );
 }
